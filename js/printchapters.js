4131bb0f56b1456ff98d45a94e2edfaf06fa41c0
Milan Gupta <milan.gupta.iitd@gmail.com>
2019-06-14 14:38:01 +0530

    Added named chapters function, and style improvements

--- js/printchapters.js
+++ js/printchapters.js
@@ -1,18 +1,31 @@
 function printChapters(){
-  var vdoChapter = [];
-  for (var i = 0; i < vdoChapters.chaptersArr.length; i++){
-    if (!Number.isNaN(Number(vdoChapters.chaptersArr[i]))){
-      vdoChapter.push(Number(vdoChapters.chaptersArr[i]));
+
+  var vdoChapterTime = [];
+  for (var i = 0; i < vdoChapters.chaptersTimeArr.length; i++){
+    if (!Number.isNaN(Number(vdoChapters.chaptersTimeArr[i]))){
+      vdoChapterTime.push(Number(vdoChapters.chaptersTimeArr[i]));
+    }
+  }
+  var vdoChapterName = [];
+  if (!vdoChapters.chaptersNameArr){
+    for (var i = 0; i < vdoChapters.chaptersTimeArr.length; i++){
+      var chapterName = 'Chapter ' + (i+1);
+      vdoChapterName.push( chapterName );
+    }
+  }
+  else {
+    for (var i = 0; i < vdoChapters.chaptersNameArr.length; i++){
+      vdoChapterName.push( vdoChapters.chaptersNameArr[i] );
     }
   }
   var htmlStr = '<div class="vdo-overlay">';
-  for (var j = 0; j < vdoChapter.length; j++ ) {
+  for (var j = 0; j < vdoChapterTime.length; j++ ) {
     htmlStr = htmlStr
       .concat('<button ')
       .concat('id="chapter').concat(String(j+1)).concat('" ')
-      .concat('mpml-on-click="seek" mpml-click-args="').concat(String(vdoChapter[j])).concat('"')
+      .concat('mpml-on-click="seek" mpml-click-args="').concat(String(vdoChapterTime[j])).concat('"')
       .concat('>')
-      .concat('Chapter ').concat(String(j+1))
+      .concat(String(vdoChapterName[j]))
       .concat('</button>');
   }
   htmlStr = htmlStr.concat('</div>');
--- vdocipher-customize.php
+++ vdocipher-customize.php
@@ -6,14 +6,15 @@
  * Author: VdoCipher
 */
 
-new VdoCipherAddShortcodeAttribute();
+new VdoCipherAddChapters();
 
-class VdoCipherAddShortcodeAttribute
+class VdoCipherAddChapters
 {
   public function __construct()
   {
     add_filter( 'vdocipher_add_shortcode_attributes', array($this, 'new_vdo_shortcode_attribute') );
-    add_action('vdocipher_customize_player', array( $this, 'customize_the_player' ) );
+    // add_action('vdocipher_customize_player', array( $this, 'insert_chapters' ) );
+    add_action('vdocipher_customize_player', array( $this, 'insert_chapters_with_names' ) );
   }
 
   public function new_vdo_shortcode_attribute($shortcode_defaults){
@@ -21,13 +22,34 @@ class VdoCipherAddShortcodeAttribute
     return $shortcode_defaults;
   }
 
-  public function customize_the_player($vdo_args){
-    $chapters = $vdo_args['chapters'];
-    if ($chapters !== '') {
-      $chapter_array = explode(',', $chapters);
+  public function insert_chapters($vdo_args){
+    $chapters_string = $vdo_args['chapters'];
+    if ( $chapters_string !== '' ) {
+      $chapter_time_array = explode(',', $chapters_string);
       wp_enqueue_script('vdo_chapters_print', plugin_dir_url(__FILE__).'js/printchapters.js');
       wp_localize_script('vdo_chapters_print', 'vdoChapters', array(
-        'chaptersArr' => $chapter_array
+        'chaptersTimeArr' => $chapter_time_array
+      ));
+    }
+  }
+
+  public function insert_chapters_with_names($vdo_args){
+    $chapters_string = $vdo_args['chapters'];
+    if ( $chapters_string !== '' ) {
+      $chapter_array = explode(';', $chapters_string);
+      $chapter_name_array = array();
+      $chapter_time_array = array();
+      foreach ( $chapter_array as $chapter ){
+        $individual_chapter = explode(',', $chapter);
+        $chapter_name = $individual_chapter[0];
+        array_push( $chapter_name_array,  $chapter_name);
+        $chapter_time = end($individual_chapter);
+        array_push( $chapter_time_array, $chapter_time );
+      }
+      wp_enqueue_script('vdo_chapters_print', plugin_dir_url(__FILE__).'js/printchapters.js');
+      wp_localize_script('vdo_chapters_print', 'vdoChapters', array(
+        'chaptersTimeArr' => $chapter_time_array,
+        'chaptersNameArr' => $chapter_name_array
       ));
     }
   }
