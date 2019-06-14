73e8f36b35089a1d15c6b0fe1d02b021f174dcc1
Milan Gupta <milan.gupta.iitd@gmail.com>
2019-06-14 13:44:23 +0530

    Working seek function, also added customize watermark class

--- js/printchapters.js
+++ js/printchapters.js
@@ -7,18 +7,18 @@ function printChapters(){
   }
   var htmlStr = '<div class="vdo-overlay">';
   for (var j = 0; j < vdoChapter.length; j++ ) {
-    htmlStr = htmlStr.concat('<button id="chapter').concat(String(j+1)).concat('">Chapter ').concat(String(j+1)).concat('</button>');
+    htmlStr = htmlStr
+      .concat('<button ')
+      .concat('id="chapter').concat(String(j+1)).concat('" ')
+      .concat('mpml-on-click="seek" mpml-click-args="').concat(String(vdoChapter[j])).concat('"')
+      .concat('>')
+      .concat('Chapter ').concat(String(j+1))
+      .concat('</button>');
   }
   htmlStr = htmlStr.concat('</div>');
 
    var v = vdo.getObjects()[0];
   v.addEventListener('mpmlLoad', () => {
     v.injectThemeHtml(htmlStr);
-    for (let k = 0; k < vdoChapter.length; k++ ) {
-      let vdoChapterStr= 'chapter'.concat(String(k+1));
-      console.log(document.getElementById(vdoChapterStr));
-      document.getElementById(vdoChapterStr).addEventListener('click', () => console.log('Arsenal'));
-      // document.getElementById(vdoChapterStr).addEventListener('click', () => v.seek(vdoChapter[k]));
-    }
   });
 }
